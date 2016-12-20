b338161edf84abcf71f170d4a8df8ab823fd858c
aaa <23456>
2016-12-20 16:38:33 +0800

    Initial Commit

---
+++ test-tomato/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test-tomato
+//
+//  Created by apple on 2016/12/20.
+//  Copyright © 2016年 zhaoyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test-tomato/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test-tomato
+//
+//  Created by apple on 2016/12/20.
+//  Copyright © 2016年 zhaoyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
